d98839c43363aca27e6ba8400bb3e8f708694b07
oal323 <iarmstr6@uncc.edu>
2023-02-22 13:02:48 -0500

    Login Front End WIP

--- client/src/pages/Login.jsx
+++ client/src/pages/Login.jsx
@@ -1,9 +1,46 @@
-import React from 'react'
+import React from 'react';
+import TextField from '@mui/material/TextField';
+import { Button } from '@mui/material';
 
-function Login() {
+const Login = () => {
+    const [userName, setUserName] = React.useState("");
+    const [pass, setPass] = React.useState("");
+    const handleClick = (e) => {
+        console.log(userName,pass);
+        setPass("");
+        setUserName("");
+    }
+    const handleUsrNameChange = (e) => {
+        setUserName(e.target.value);
+    }
+    const handlePassChange = (e) => {
+        setPass(e.target.value);
+    }
     return (
-        <div>
+        <div style={{ display: 'flex', flexDirection: 'column', justifyContent: 'center', alignItems: 'center' }}>
 
+            <TextField
+                id="user-name"
+                label="User Name"
+                variant="outlined"
+                margin="normal"
+                value={userName}
+                onChange={handleUsrNameChange} 
+                
+            />
+
+            <TextField
+                id="password"
+                label="Password"
+                variant="outlined"
+                margin="normal"
+                type = "password"
+                value={pass}
+                onChange={handlePassChange}
+
+            />
+
+            <Button varient="contained" onClick={handleClick}>Login</Button>
         </div>
     )
 }
